57afeefb914238bdd43c848ae084d492318d780b
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:51 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,8 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.materialicons.MaterialIconsPageConfigurator;
-
 module com.jwebmp.plugins.materialicons {
 
 	exports com.jwebmp.plugins.materialicons;
@@ -15,10 +10,10 @@ module com.jwebmp.plugins.materialicons {
 	requires java.logging;
 	requires com.jwebmp.guicedinjection;
 
-	provides IPageConfigurator with MaterialIconsPageConfigurator;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.materialicons.MaterialIconsPageConfigurator;
 
-	provides IGuiceScanModuleExclusions with com.jwebmp.plugins.materialicons.implementations.MaterialIconsExclusionsModule;
-	provides IGuiceScanJarExclusions with com.jwebmp.plugins.materialicons.implementations.MaterialIconsExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.materialicons.implementations.MaterialIconsExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.materialicons.implementations.MaterialIconsExclusionsModule;
 
 	opens com.jwebmp.plugins.materialicons to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
