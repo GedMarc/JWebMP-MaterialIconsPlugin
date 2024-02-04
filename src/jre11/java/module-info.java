9d8e4551df8e8dc16afe01ae7889b512a81f2131
Marc Magon <marc.magon@gmail.com>
2024-02-04 23:43:36 +0200

    Updates for version 2

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -5,7 +5,7 @@ module com.jwebmp.plugins.materialicons {
 	exports com.jwebmp.plugins.materialicons;
 
 	requires com.jwebmp.core;
-	requires com.guicedee.logmaster;
+	
 
 	requires jakarta.validation;
 	requires java.logging;
